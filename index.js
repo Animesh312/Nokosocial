b69dc5b6af70aad90fef0c9c629532952d42239a
Animesh Wankhede <88401055+Animesh312@users.noreply.github.com>
2022-12-26 18:20:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // sidebar
 const menuItems=document.querySelectorAll('.menu-item');
 
+// messagess
+const messagesNotification = document.querySelector('#messages-notification');
+const messages= document.querySelector('.messages');
+const message=messages.querySelector('.message');
+const messageSearch=document.querySelector('#message-search');
+
 const changeActiveItem =()=>{
     menuItems.forEach(item=>{
         item.classList.remove('active');
@@ -19,6 +25,35 @@ menuItems.forEach(item=>{
         else{
             document.querySelector('.notifications-popup').
             style.display='block';
+            document.querySelector('#notifications .notificaton-count').style.display='none';
         }
     })
+})
+
+// ==============messages======
+// search chat
+.messageSearch.addEventListener('keyup',searchMessage){
+    const val=messageSearch.ariaValueMax.toLowerCase();
+    message.forEach(chat=>{
+        console.log(val);
+        let name=chat.querySelector('h5').textContent.toLowerCase();
+        if(name.indexOf(val)!=-1){
+            chat.style.display='flex';
+        }
+        else{
+            chat.style.display='none';
+        }
+    })
+}
+
+
+// highlight message card 
+messagesNotification.addEventListener('click',()=>{
+    messages.style.boxShadow='0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector('.notification-count').style.display='none';
+    setTimeout(() => {
+        messages.style.boxShadow='none';
+    }, 2000); 
+   
+   
 })
